63f23587618b32778a6bae8c06f591e31a6fe0ee
10mi8o <totoma855@gmail.com>
2020-09-14 16:28:27 +0900

    各ページにLayoutコンポーネントをimport

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Post from '../components/blog/Post' 
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 import { fetchAllPosts } from '../services/blog'
@@ -7,21 +8,17 @@ import { fetchAllPosts } from '../services/blog'
 export default function Home(props) {
   const posts = props.posts
   return (
-    <div>
-      <Head>
-        <title>MemoCached</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>
-          { posts.map((post, id) => {
-            return(
-              <React.Fragment key={id}> 
-                <Post title={post.fields.title} body={post.fields.body} slug={post.fields.slug} /> 
-              </React.Fragment>
-            )
-          })}
-      </div>
-    </div>
+      <Layout>
+        <div>
+            { posts.map((post, id) => {
+              return(
+                <React.Fragment key={id}> 
+                  <Post title={post.fields.title} body={post.fields.body} slug={post.fields.slug} /> 
+                </React.Fragment>
+              )
+            })}
+        </div>
+      </Layout>
   )
 }
 
