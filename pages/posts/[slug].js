334ab8691ef0ed53ed0bf10794f444e44eca05e0
10mi8o <totoma855@gmail.com>
2020-09-14 13:34:03 +0900

    記事詳細のベース作成

---
+++ pages/posts/[slug].js
@@ -0,0 +1,36 @@
+import { fetchAllPosts, fetchPostBySlug } from '../../services/blog'
+import Post from '../../components/blog/Post'
+
+export default function PostDetail(props) {
+  const post = props.post.items
+  
+  return(
+    <>
+      <Post title={post[0].fields.title} body={post[0].fields.body} />
+    </>
+  )
+}
+
+export async function getStaticProps({params}) {
+
+  const slug = params.slug
+
+  const post = await fetchPostBySlug(slug)
+  return {
+    props: {
+      post: post
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = await fetchAllPosts()
+  const paths = posts.map((post) => {
+      return `/posts/${post.fields.slug}`
+    })
+
+  return {
+    paths: paths,
+    fallback: false
+  }
+}
