274c0c387a47105c0ee18793bdd3845611542cd7
10mi8o <totoma855@gmail.com>
2020-09-17 21:53:38 +0900

    Category,Tagで記事を抽出するメソッドを追記

--- services/blog.js
+++ services/blog.js
@@ -26,3 +26,40 @@ export async function fetchPostBySlug(slug) {
   })
   return post
 }
+
+// slugで一致するCategoryを抽出
+export async function fetchCategory(slug) {
+  const category = await client.getEntries({
+    content_type: 'category',
+    'fields.slug': slug
+  })
+  return category
+}
+
+// slugで一致するTagを抽出
+export async function fetchTag(slug) {
+  const tag = await client.getEntries({
+    content_type: 'tag',
+    'fields.slug': slug
+  })
+  return tag
+}
+
+// Categoryで記事を抽出
+export async function fetchPostByCategory(id) {
+  const posts = await client.getEntries({
+    content_type: 'blogPost',
+    'fields.category.sys.id': id
+  })
+  // console.log(posts.items[0].fields.category.fields.slug)
+  return posts
+}
+
+// Tagで記事を抽出
+export async function fetchPostByTag(id) {
+  const posts = await client.getEntries({
+    content_type: 'blogPost',
+    'fields.tag.sys.id': id
+  })
+  return posts
+}
