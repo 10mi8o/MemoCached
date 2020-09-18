427cad381fbc07e23806caddc1450a1e758ae222
10mi8o <totoma855@gmail.com>
2020-09-19 07:17:27 +0900

    サイトタイトル名にルートへのリンク追記

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,8 +1,16 @@
+import Link from 'next/link'
+
 const Header = () => {
   return(
     <>
       <header className="py-10 text-center">
-        <h1 className="font-black line-through">Memo Cached</h1>
+        <h1 className="font-black line-through">
+          <Link href="/">
+            <a href="">
+              Memo Cached
+            </a>
+          </Link>
+        </h1>        
       </header>
     </>
   )
