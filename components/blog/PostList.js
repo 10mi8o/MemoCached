1becc242000c0e8df7e89c1c7a700d9a7740071f
10mi8o <totoma855@gmail.com>
2020-09-18 11:17:56 +0900

    一覧表示のコンポーネント追加

--- components/blog/PostList.js
+++ components/blog/PostList.js
@@ -1,11 +1,13 @@
 const PostList = ({title, img_url}) => {
   return(
-    <>
-      <img src={img_url} alt=""/>
-      <h2>
+    <div>
+      <figure>
+        <img className="w-full" src={img_url} alt=""/>
+      </figure>
+      <h2 className="font-bold text-xl mb-2 px-6 py-4">
         {title}
       </h2>
-    </>
+    </div>
   )
 }
 
