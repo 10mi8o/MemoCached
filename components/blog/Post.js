62531fad4d2a9233ce74f7380efce5c1ad9b7df8
10mi8o <totoma855@gmail.com>
2020-09-19 05:36:37 +0900

    マークダウンパース時、code部分はシンタックスハイライトが効くよう変更

--- components/blog/Post.js
+++ components/blog/Post.js
@@ -1,15 +1,21 @@
 import Link from 'next/link'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+const CodeBlock = ({ language, value }) => {
+  return <SyntaxHighlighter language="javascript" style={darcula}>{value}</SyntaxHighlighter>;
+};
 
 const Post = ({ title, body, image }) => {
-  
+
   return(
     <>
       <figure>
         <img src={image.file.url} alt={image.description}/>
       </figure>
       <h1>{title}</h1>
-      <ReactMarkdown source={body}/>
+      <ReactMarkdown source={body} renderers={{ code: CodeBlock }}/>
     </>
   )
 }
