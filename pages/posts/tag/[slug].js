312e5b5395515833e88fd8cc459bcc6edb43d415
10mi8o <totoma855@gmail.com>
2020-09-21 17:13:00 +0900

    一覧へ戻るボタンのスタイル変更

--- pages/posts/tag/[slug].js
+++ pages/posts/tag/[slug].js
@@ -35,7 +35,7 @@ export default function PostDetail(props) {
       })}
       <Link href="/">
         <a>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-12 rounded">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 my-12 border border-gray-400 rounded shadow">
             一覧へ戻る
           </button>
         </a>
