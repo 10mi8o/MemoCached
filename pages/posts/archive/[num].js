fd8ec8974d2713a64d862d45b25d924227842d03
10mi8o <totoma855@gmail.com>
2020-09-24 21:34:51 +0900

    deploy検証

--- pages/posts/archive/[num].js
+++ pages/posts/archive/[num].js
@@ -7,47 +7,48 @@
 
 // const COUNT_PER_PAGE = 3
 
-// export default function PostDetail(props) {
-//   const posts = props.posts
+export default function PostDetail(props) {
+  // const posts = props.posts
 
-//   return(
-//     <Layout>
-//       <div className="container m-auto py-10">
-//         <div className="flex flex-wrap">
-//           {posts.map((post, id) => (
-//             <div key={id} className="w-1/3 px-2 mb-10 flex">
-//               <div className="max-w-sm rounded overflow-hidden shadow-lg">
-//                 <Link  href="/posts/[slug]" as={`/posts/${post.fields.slug}`}>
-//                   <a>
-//                     <PostList  title={post.fields.title} createdAt={post.sys.createdAt} img_url={post.fields.image.fields.file.url} />
-//                   </a>
-//                 </Link>
-//                 <div className="flex px-4 py-4">
-//                 {post.fields.tag.map((tag_name, id)=> (
-//                     <div key={id}>
-//                       <Link href="/posts/tag/[slug]" as={`/posts/tag/${tag_name.fields.slug}`}>
-//                       <a>
-//                         <Label name={tag_name.fields.name} />
-//                       </a>
-//                       </Link>
-//                     </div>
-//                   )
-//                 )}
-//               </div>
-//               </div>
-//             </div>
-//             )
-//           )}
-//         </div>
-//         <Pager
-//             page={props.page} total={props.total} perPage={props.perPage}
-//             href="/posts/archive/[num]"
-//             asCallback={(page) => `/posts/archive/${page}`}
-//           />
-//       </div>
-//     </Layout>
-//   )
-// }
+  return(
+    <Layout>
+      test
+      {/* <div className="container m-auto py-10">
+        <div className="flex flex-wrap">
+          {posts.map((post, id) => (
+            <div key={id} className="w-1/3 px-2 mb-10 flex">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <Link  href="/posts/[slug]" as={`/posts/${post.fields.slug}`}>
+                  <a>
+                    <PostList  title={post.fields.title} createdAt={post.sys.createdAt} img_url={post.fields.image.fields.file.url} />
+                  </a>
+                </Link>
+                <div className="flex px-4 py-4">
+                {post.fields.tag.map((tag_name, id)=> (
+                    <div key={id}>
+                      <Link href="/posts/tag/[slug]" as={`/posts/tag/${tag_name.fields.slug}`}>
+                      <a>
+                        <Label name={tag_name.fields.name} />
+                      </a>
+                      </Link>
+                    </div>
+                  )
+                )}
+              </div>
+              </div>
+            </div>
+            )
+          )}
+        </div>
+        <Pager
+            page={props.page} total={props.total} perPage={props.perPage}
+            href="/posts/archive/[num]"
+            asCallback={(page) => `/posts/archive/${page}`}
+          />
+      </div> */}
+    </Layout>
+  )
+}
 
 // export async function getStaticProps({params}) {
 
