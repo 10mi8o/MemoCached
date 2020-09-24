4cca3f9969e42884829968ef9bd5d129a0087a42
10mi8o <totoma855@gmail.com>
2020-09-25 00:04:49 +0900

    Card_UIをリファクタ

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout'
 import PostList from '../components/blog/PostList'
 import Label from '../components/common/Label'
 import { fetchAllPosts } from '../services/blog'
+import Card from '../components/common/molecules/Card'
 
 //記事一覧ページ
 export default function Home(props) {
@@ -13,35 +14,7 @@ export default function Home(props) {
     <Layout>
       <div className="container m-auto">
         <div className="flex flex-wrap ">
-        { posts.map((post, id) => (   
-            <div key={id} className="w-1/3 px-2 mb-10 flex">
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
-              <Link  href="/posts/[slug]" as={`/posts/${post.fields.slug}`}>
-                <a>
-                  <PostList 
-                    title={post.fields.title} 
-                    createdAt={post.sys.createdAt} 
-                    img_url={post.fields.image?.fields.file.url === undefined ? "" : post.fields.image.fields.file.url}
-                    category={post.fields.category.fields.slug}
-                  />
-                </a>
-              </Link>
-                <div className="flex px-4 py-4">
-                  {post.fields.tag.map((tag_name, id)=> (
-                    <div key={id}>
-                      <Link href="/posts/tag/[slug]" as={`/posts/tag/${tag_name.fields.slug}`}>
-                      <a>
-                        <Label name={tag_name.fields.name} />
-                      </a>
-                      </Link>
-                    </div>
-                  )
-                  )}
-                </div>
-              </div>
-            </div>
-          )
-        )}
+          <Card props={posts}/>
         </div>
         { LIMIT <= 16 ? '' :
         <div className="text-center">
