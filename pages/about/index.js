329ad4f40abf274e26e781597b40d17095c41842
10mi8o <totoma855@gmail.com>
2020-10-03 23:26:10 +0900

    aboutページにicon追加

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,15 @@
 import Layout from 'components/Layout'
+import { 
+  SiQiita, 
+  SiSpotify, 
+  SiPinterest, 
+  SiPhp, 
+  SiLaravel, 
+  SiJavascript, 
+  SiTypescript, 
+  SiHtml5, 
+  SiCss3 
+} from "react-icons/si";
 
 export default function About() {
   return(
@@ -6,7 +17,44 @@ export default function About() {
       <div id="about" className="container m-auto px-8 lg:px-0">
         <main className="md:flex">
           <section className="right_box">
-            <div className="mb-5">
+            <div　className="mb-5">
+              <div className="heading">
+                Profile
+              </div>
+              <img className="icon" src="/images/icon.png" alt=""/>
+              <p className="border-b-2 border-gray-600 pb-5">
+                <span className="font-bold">富山 健作</span><br />
+                都内在住のWebエンジニアです。仕事では主にWebアプリケーションの開発やフロントエンドの技術を用いるWeb制作に携わっています。
+                フロントからサーバーまで書きますが、現在は、フロントエンド開発のスキルを上げるべく、キャッチアップ中。
+                最近は、UXデザインに興味あり。
+              </p>
+              <div className="flex items-center react-icons">
+                Skill...
+                <SiPhp size="30"/>
+                <SiLaravel size="30"/>
+                <SiJavascript size="30"/>
+                <SiTypescript size="30"/>
+                <SiHtml5 size="30"/>                
+                <SiCss3 size="30"/>
+              </div>
+
+              <div className="flex items-center react-icons">
+                Others...
+                <a href="https://qiita.com/10mi8o" target="_blank">
+                  <SiQiita size="30"/>
+                </a>
+                <a href="https://www.pinterest.com/10mi/_saved/" target="_blank">
+                  <SiPinterest size="30"/>
+                </a>
+                <a href="https://open.spotify.com/user/totoma855?si=ZBneslMGR86Dh21jL4bKmw" target="_blank">
+                  <SiSpotify size="30"/>
+                </a>
+              </div>
+            </div>
+
+          </section>
+          <section className="left_box">
+          <div className="mb-5">
               <div className="heading">
                 About this Site
               </div>
@@ -16,44 +64,10 @@ export default function About() {
                 QiitaやNoteに書くほどではないけど、覚えておきたい事、後で参照したいトピックなどを貯めていく場として運営しています。
               </p>
             </div>
-
-            <div　className="mb-5">
-              <div className="heading">
-                Profile
-              </div>
-              {/* <img className="icon" src="/images/icon.png" alt=""/> */}
-              <p>
-                <span className="font-bold">富山 健作</span><br />
-                都内在住のWebエンジニアです。フロントからサーバーまで書きます。
-                最近は、UXデザインに興味あり。
-              </p>
-            </div>
-
-            {/* <div>
-              <div className="heading">
-                Skill
-              </div>
-              <ul>
-                <li>PHP</li>
-                <li>Python</li>
-                <li>JavaScript</li>
-                <li>TypeScript</li>
-              </ul>
-              <ul>
-                <li>Laravel</li>
-                <li>Django</li>
-                <li>React</li>
-                <li>Riot.js</li>
-              </ul>
-            </div> */}
-          </section>
-          <section className="left_box">
-            <img src="/images/sheep.png" alt=""/>
           </section>
 
         </main>
       </div>
-
     </Layout>
   )
 }
