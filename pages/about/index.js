9e4105695c2df02a59d26f6aa9aebfad99b63475
10mi8o <totoma855@gmail.com>
2020-10-04 12:11:56 +0900

    [微調整]Aboutページに情報追記、styleSheet追記

--- pages/about/index.js
+++ pages/about/index.js
@@ -8,7 +8,10 @@ import {
   SiJavascript, 
   SiTypescript, 
   SiHtml5, 
-  SiCss3 
+  SiCss3,
+  SiReact,
+  SiGraphql, 
+  SiGo,
 } from "react-icons/si";
 
 export default function About() {
@@ -21,7 +24,9 @@ export default function About() {
               <div className="heading">
                 Profile
               </div>
-              <img className="icon" src="/images/icon.png" alt=""/>
+              <div className="w-1/2 m-auto">
+                <img className="icon" src="/images/icon.png" alt=""/>
+              </div>
               <p className="border-b-2 border-gray-600 pb-5">
                 <span className="font-bold">富山 健作</span><br />
                 都内在住のWebエンジニアです。仕事では主にWebアプリケーションの開発やフロントエンドの技術を用いるWeb制作に携わっています。
@@ -30,12 +35,37 @@ export default function About() {
               </p>
               <div className="flex items-center react-icons">
                 Skill...
-                <SiPhp size="30"/>
-                <SiLaravel size="30"/>
-                <SiJavascript size="30"/>
-                <SiTypescript size="30"/>
-                <SiHtml5 size="30"/>                
-                <SiCss3 size="30"/>
+                <a href="https://www.php.net/manual/ja/index.php" target="_blank">
+                  <SiPhp size="30"/>
+                </a>
+                <a href="https://readouble.com/laravel/" target="_blank">
+                  <SiLaravel size="30"/>
+                </a>
+                <a href="https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference" target="_blank">
+                  <SiJavascript size="30"/>
+                </a>
+                <a href="https://www.typescriptlang.org/docs/" target="_blank">
+                  <SiTypescript size="30"/>
+                </a>
+                <a href="https://developer.mozilla.org/ja/docs/Web/HTML/Reference" target="_blank">
+                  <SiHtml5 size="30"/>                
+                </a>
+                <a href="https://developer.mozilla.org/ja/docs/Web/CSS/Reference" target="_blank">
+                  <SiCss3 size="30"/>
+                </a>
+              </div>
+
+              <div className="flex items-center react-icons">
+                Studying...
+                <a href="https://ja.reactjs.org/" target="_blank">
+                  <SiReact size="30"/>
+                </a>
+                <a href="https://graphql.org/" target="_blank">
+                  <SiGraphql size="30"/>
+                </a>
+                <a href="https://golang.org/" target="_blank">
+                  <SiGo size="30"/>
+                </a>
               </div>
 
               <div className="flex items-center react-icons">
@@ -54,16 +84,19 @@ export default function About() {
 
           </section>
           <section className="left_box">
-          <div className="mb-5">
-              <div className="heading">
-                About this Site
-              </div>
-              <p>
-                学習したトピックの忘備録兼、ナレッジシェアの場として書いているブログです。
-                <br />
-                QiitaやNoteに書くほどではないけど、覚えておきたい事、後で参照したいトピックなどを貯めていく場として運営しています。
-              </p>
-            </div>
+            <div className="mb-5">
+                <div className="heading">
+                  About this Site
+                </div>
+                <p>
+                  学習したトピックの忘備録兼、ナレッジシェアの場として書いているブログです。
+                  <br />
+                  QiitaやNoteに書くほどではないけど、覚えておきたい事、後で参照したいトピックなどを貯めていく場として運営しています。
+                </p>
+                <p>
+                  このブログは、Next.js, Contentful, Vercelで作っています。
+                </p>
+            </div>            
           </section>
 
         </main>
